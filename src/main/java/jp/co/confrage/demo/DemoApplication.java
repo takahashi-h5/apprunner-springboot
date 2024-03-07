cc2b79d9f339bcc55401bb9c0a6e66898b081b07
takahashi-h5 <mori@confrage.com>
2024-03-07 12:32:33 +0900

    first commit

---
+++ src/main/java/jp/co/confrage/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package jp.co.confrage.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/get")
+    public String get() {
+        return "get";
+    }
+
+	@PostMapping("/post")
+	public String post() {
+        return "post";
+	}
+}
